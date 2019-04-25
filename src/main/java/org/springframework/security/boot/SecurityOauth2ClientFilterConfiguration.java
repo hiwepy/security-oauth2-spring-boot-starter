598ce2b46a7a102a25e8d072f900c6323b216ac3
wandl <hnxyhcwdl1003@163.com>
2019-04-25 22:54:40 +0800

    x

--- src/main/java/org/springframework/security/boot/SecurityOauth2ClientFilterConfiguration.java
+++ src/main/java/org/springframework/security/boot/SecurityOauth2ClientFilterConfiguration.java
@@ -1,10 +1,5 @@
 package org.springframework.security.boot;
 
-import java.util.Arrays;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
@@ -15,46 +10,16 @@ import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationDetailsSource;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.boot.biz.authentication.ajax.AjaxAwareAuthenticationFailureHandler;
-import org.springframework.security.boot.biz.authentication.ajax.AjaxAwareAuthenticationSuccessHandler;
-import org.springframework.security.boot.biz.filter.CustomCorsFilter;
-import org.springframework.security.boot.jwt.authentication.ajax.AjaxUsernamePasswordAuthenticationFilter;
-import org.springframework.security.boot.jwt.authentication.jwt.JwtTokenAuthenticationFilter;
-import org.springframework.security.boot.jwt.authentication.jwt.SkipPathRequestMatcher;
-import org.springframework.security.boot.jwt.authentication.jwt.extractor.TokenExtractor;
-import org.springframework.security.boot.utils.StringUtils;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.authentication.NullRememberMeServices;
-import org.springframework.security.web.authentication.RememberMeServices;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 @AutoConfigureBefore(name = { "org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration",
@@ -82,12 +47,10 @@ public class SecurityOauth2ClientFilterConfiguration<OAuth2RestTemplate> extends
 	protected BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
- 	@Bean
- 	public OAuth2RestOperations restTemplate(OAuth2ClientContext oauth2ClientContext) {
- 		return new OAuth2RestTemplate(remote(), oauth2ClientContext);
- 	}
-
+	/*
+	 * @Bean public OAuth2RestTemplate restTemplate(OAuth2ClientContext
+	 * oauth2ClientContext) { return new OAuth2RestTemplate( oauth2ClientContext); }
+	 */
 	
 	@Bean
 	@ConditionalOnMissingBean
@@ -113,216 +76,20 @@ public class SecurityOauth2ClientFilterConfiguration<OAuth2RestTemplate> extends
 		return template;
 	}*/
 
-	@Bean
-	@ConditionalOnMissingBean
-	public AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource() {
-		return new WebAuthenticationDetailsSource();
-	}
-
-	@Bean
-	@ConditionalOnMissingBean
-	public AuthenticationSuccessHandler successHandler(ObjectMapper mapper) {
-
-		// Ajax Login
-		if (bizProperties.isLoginAjax()) {
-			AjaxAwareAuthenticationSuccessHandler successHandler = new AjaxAwareAuthenticationSuccessHandler(mapper,
-					jwtProperties);
-			return successHandler;
-		}
-		// Form Login
-		else {
-			SimpleUrlAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-			successHandler.setDefaultTargetUrl(bizProperties.getSuccessUrl());
-			return successHandler;
-		}
-
-	}
-
-	@Bean
-	@ConditionalOnMissingBean
-	public AuthenticationFailureHandler failureHandler() {
-		// Ajax Login
-		if (bizProperties.isLoginAjax()) {
-			return new AjaxAwareAuthenticationFailureHandler(bizProperties.getFailureUrl());
-		}
-		// Form Login
-		else {
-			return new SimpleUrlAuthenticationFailureHandler(bizProperties.getFailureUrl());
-		}
-	}
-
-	@Bean
-	@ConditionalOnMissingBean
-	public SessionAuthenticationStrategy sessionStrategy() {
-		return new NullAuthenticatedSessionStrategy();
-	}
-
-	@Bean
-	@ConditionalOnMissingBean
-	public RememberMeServices rememberMeServices() {
-		return new NullRememberMeServices();
-	}
-
-	public static final String JWT_TOKEN_HEADER_PARAM = "X-Authorization";
-	public static final String FORM_BASED_LOGIN_ENTRY_POINT = "/api/auth/login";
-	public static final String TOKEN_BASED_AUTH_ENTRY_POINT = "/api/**";
-	public static final String TOKEN_REFRESH_ENTRY_POINT = "/api/auth/token";
-
-	@Bean
-	@ConditionalOnMissingBean
-	public ObjectMapper objectMapper() {
-		return new ObjectMapper();
-	}
-
-	@Bean
-	@ConditionalOnMissingBean
-	public AjaxUsernamePasswordAuthenticationFilter jwtAjaxLoginProcessingFilter(
-			AuthenticationFailureHandler failureHandler, AuthenticationManager authenticationManager,
-			ApplicationEventPublisher publisher,
-			AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource,
-			AuthenticationSuccessHandler successHandler, RememberMeServices rememberMeServices,
-			SessionAuthenticationStrategy sessionStrategy, ObjectMapper objectMapper) throws Exception {
-		// AjaxUsernamePasswordAuthenticationFilter filter = new
-		// AjaxUsernamePasswordAuthenticationFilter(FORM_BASED_LOGIN_ENTRY_POINT,
-		// successHandler, failureHandler, objectMapper);
-		// filter.setAuthenticationManager(authenticationManager);
-		return null;
-	}
-
-	@Bean
-	@ConditionalOnMissingBean
-	public JwtTokenAuthenticationFilter jwtTokenAuthenticationProcessingFilter(
-			AuthenticationFailureHandler failureHandler, TokenExtractor tokenExtractor,
-			AuthenticationManager authenticationManager, ApplicationEventPublisher publisher,
-			AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource,
-			AuthenticationSuccessHandler successHandler, RememberMeServices rememberMeServices,
-			SessionAuthenticationStrategy sessionStrategy) throws Exception {
-
-		List<String> pathsToSkip = Arrays.asList(TOKEN_REFRESH_ENTRY_POINT, FORM_BASED_LOGIN_ENTRY_POINT);
-		SkipPathRequestMatcher matcher = new SkipPathRequestMatcher(pathsToSkip, TOKEN_BASED_AUTH_ENTRY_POINT);
-
-		JwtTokenAuthenticationFilter authenticationFilter = new JwtTokenAuthenticationFilter(failureHandler,
-				tokenExtractor, matcher);
-
-		authenticationFilter.setAllowSessionCreation(false);
-		authenticationFilter.setApplicationEventPublisher(publisher);
-		authenticationFilter.setAuthenticationDetailsSource(authenticationDetailsSource);
-		authenticationFilter.setAuthenticationFailureHandler(failureHandler);
-		authenticationFilter.setAuthenticationManager(authenticationManager);
-		authenticationFilter.setAuthenticationSuccessHandler(successHandler);
-		authenticationFilter.setContinueChainBeforeSuccessfulAuthentication(false);
-		if (StringUtils.hasText(bizProperties.getLoginUrlPatterns())) {
-			authenticationFilter.setFilterProcessesUrl(bizProperties.getLoginUrlPatterns());
-		}
-		// authenticationFilter.setMessageSource(messageSource);
-		authenticationFilter.setRememberMeServices(rememberMeServices);
-		authenticationFilter.setSessionAuthenticationStrategy(sessionStrategy);
-
-		return authenticationFilter;
-	}
-
-	@Bean
-	@ConditionalOnMissingBean
-	public AbstractAuthenticationProcessingFilter authenticationFilter(AuthenticationFailureHandler failureHandler,
-			AuthenticationManager authenticationManager, ApplicationEventPublisher publisher,
-			AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource,
-			AuthenticationSuccessHandler successHandler, RememberMeServices rememberMeServices,
-			SessionAuthenticationStrategy sessionStrategy) {
-
-		UsernamePasswordAuthenticationFilter authenticationFilter = new UsernamePasswordAuthenticationFilter();
-
-		authenticationFilter.setAllowSessionCreation(bizProperties.isAllowSessionCreation());
-		authenticationFilter.setApplicationEventPublisher(publisher);
-		authenticationFilter.setAuthenticationDetailsSource(authenticationDetailsSource);
-		authenticationFilter.setAuthenticationFailureHandler(failureHandler);
-		authenticationFilter.setAuthenticationManager(authenticationManager);
-		authenticationFilter.setAuthenticationSuccessHandler(successHandler);
-		authenticationFilter.setContinueChainBeforeSuccessfulAuthentication(false);
-		if (StringUtils.hasText(bizProperties.getLoginUrlPatterns())) {
-			authenticationFilter.setFilterProcessesUrl(bizProperties.getLoginUrlPatterns());
-		}
-		// authenticationFilter.setMessageSource(messageSource);
-		authenticationFilter.setPasswordParameter(bizProperties.getPasswordParameter());
-		authenticationFilter.setPostOnly(bizProperties.isPostOnly());
-		authenticationFilter.setRememberMeServices(rememberMeServices);
-		authenticationFilter.setSessionAuthenticationStrategy(sessionStrategy);
-		authenticationFilter.setUsernameParameter(bizProperties.getUsernameParameter());
-
-		return authenticationFilter;
-	}
-
-	@Bean
-	@ConditionalOnMissingBean
-	public AuthenticationEntryPoint authenticationEntryPoint() {
-
-		LoginUrlAuthenticationEntryPoint entryPoint = new LoginUrlAuthenticationEntryPoint(bizProperties.getLoginUrl());
-		entryPoint.setForceHttps(bizProperties.isForceHttps());
-		entryPoint.setUseForward(bizProperties.isUseForward());
-
-		return entryPoint;
-	}
-
-	/**
-	 * 系统登录注销过滤器；默认：org.springframework.security.web.authentication.logout.LogoutFilter
-	 */
-	@Bean
-	@ConditionalOnMissingBean
-	public LogoutFilter logoutFilter() {
-		// 登录注销后的重定向地址：直接进入登录页面
-		LogoutFilter logoutFilter = new LogoutFilter(bizProperties.getLoginUrl(), new SecurityContextLogoutHandler());
-		logoutFilter.setFilterProcessesUrl(bizProperties.getLogoutUrlPatterns());
-		return logoutFilter;
-	}
-
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(ajaxAuthenticationProvider);
-        auth.authenticationProvider(jwtAuthenticationProvider);
+        //auth.authenticationProvider(ajaxAuthenticationProvider);
+       // auth.authenticationProvider(jwtAuthenticationProvider);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	http
-        .csrf().disable() // We don't need CSRF for JWT based authentication
-        .exceptionHandling()
-        .authenticationEntryPoint(this.authenticationEntryPoint)
-        
-        .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-        .and()
-            .authorizeRequests()
-                .antMatchers(bizProperties.getLoginUrlPatterns()).permitAll() // Login end-point
-                .antMatchers(TOKEN_REFRESH_ENTRY_POINT).permitAll() // Token refresh end-point
-                .antMatchers("/console").permitAll() // H2 Console Dash-board - only for testing
-        .and()
-            .authorizeRequests()
-                .antMatchers(TOKEN_BASED_AUTH_ENTRY_POINT).authenticated() // Protected API End-points
-        .and()
-            .addFilterBefore(new CustomCorsFilter(), UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(jwtAjaxLoginProcessingFilter, UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(jwtTokenAuthenticationProcessingFilter, UsernamePasswordAuthenticationFilter.class);
     }
      
-    
-	/*
-	 * @Bean public FilterRegistrationBean<HttpParamsFilter> httpParamsFilter() {
-	 * FilterRegistrationBean<HttpParamsFilter> filterRegistrationBean = new
-	 * FilterRegistrationBean<HttpParamsFilter>();
-	 * filterRegistrationBean.setFilter(new HttpParamsFilter());
-	 * filterRegistrationBean.setOrder(-999);
-	 * filterRegistrationBean.addUrlPatterns("/"); return filterRegistrationBean; }
-	 */
-
+     
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		this.applicationContext = applicationContext;
 	}
-
-	public ApplicationContext getApplicationContext() {
-		return applicationContext;
-	}
-
+ 
 }
